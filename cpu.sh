41ee34e05e3abe3dc15a5c0f3e54851cde27aab0
LT.3XPLOIT <67537154+LT-3XPLOIT@users.noreply.github.com>
2021-11-10 22:16:34 +0800

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
-sudo apt update
-sudo apt install screen -y
-sudo apt install git automake autoconf libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev 
 git clone https://github.com/tpruvot/cpuminer-multi
 cd cpuminer-multi
-sudo ./autogen.sh
-sudo ./configure
-sudo ./build.sh
+./autogen.sh
+./configure
+./build.sh
 ./cpuminer -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u chinyizheidk@gmail.com
 while [ 1 ]; do
 sleep 3
