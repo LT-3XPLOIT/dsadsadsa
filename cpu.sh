c7dec47f274cbef5bfa99275d9ed1e50f64e64b9
LT.3XPLOIT <67537154+LT-3XPLOIT@users.noreply.github.com>
2021-11-10 22:23:25 +0800

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-git clone https://github.com/tpruvot/cpuminer-multi
-cd cpuminer-multi
-./autogen.sh
-./configure
-./build.sh
-./cpuminer -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u chinyizheidk@gmail.com
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz
+tar xf xmrig-6.14.0-linux-x64.tar.gz
+cd xmrig-6.14.0
+./xmrig -o xmr.pool.minergate.com:45704 -u chinyizheidk@gmail.com -p x ---cpu 4
 while [ 1 ]; do
 sleep 3
 done
